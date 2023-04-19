5d76f49fb7e04fc63568b03a90182418b6100a48
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-04-19 19:15:04 -0300

    melhorando mais a parte de menu

--- menu/menu.py
+++ menu/menu.py
@@ -12,4 +12,5 @@ def menu():
 #    # #    #   #   #    # #      #      #    # #   #   
 #####   ####    #    ####  #      ######  ####  #   #   
                                                         {Style.RESET_ALL}
-	"""
+	Version -> {Fore.YELLOW}1.0.0 {Style.RESET_ALL}
+	Author -> {Fore.GREEN}Zorro{Style.RESET_ALL}"""
